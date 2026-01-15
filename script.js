3802c7bbf0b7203d1bc8b48e15cc0ecead0018cb
Sungket <sungket_rhymes@hotmail.com>
2026-01-15 15:32:14 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let gameboard = [[A1, A2, A3], [A4, A5, A6], [A7, A8, A9]];
+
+let A1 = document.getElementById("A1");
+let A2 = document.getElementById("A2");
+let A3 = document.getElementById("A3");
+
+let A4 = document.getElementById("B1");
+let A5 = document.getElementById("B2");
+let A6 = document.getElementById("B3");
+
+let A7 = document.getElementById("C1");
+let A8 = document.getElementById("C2");
+let A9 = document.getElementById("C3");
+
+
+function changeElement(input, location) {
+    location = input;
+}
+
+function winningCombination () {
+
+}
