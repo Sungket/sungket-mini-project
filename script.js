0bc64bdb3c107c3eaaa0228f0cd233a378a8df47
Sungket <sungket_rhymes@hotmail.com>
2026-01-16 09:04:47 +0000

    commented out the html table and added in divs to form the game grid

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let gameboard = [[A1, A2, A3], [A4, A5, A6], [A7, A8, A9]];
+// let gameboard = [[A00, A01, A02],
+//                  [A10, A11, A12], 
+//                  [A20, A21, A22]];
 
-let A1 = document.getElementById("A1");
-let A2 = document.getElementById("A2");
-let A3 = document.getElementById("A3");
+// let A00 = document.getElementById("A1");
+// let A01 = document.getElementById("A2");
+// let A02 = document.getElementById("A3");
 
-let A4 = document.getElementById("B1");
-let A5 = document.getElementById("B2");
-let A6 = document.getElementById("B3");
+// let A10 = document.getElementById("B1");
+// let A11 = document.getElementById("B2");
+// let A12 = document.getElementById("B3");
 
-let A7 = document.getElementById("C1");
-let A8 = document.getElementById("C2");
-let A9 = document.getElementById("C3");
+// let A20 = document.getElementById("C1");
+// let A21 = document.getElementById("C2");
+// let A22 = document.getElementById("C3");
 
+const box = document.getElementById("box");
 
-function changeElement(input, location) {
-    location = input;
+function addImage() {
+    
 }
 
-function winningCombination () {
-
-}
