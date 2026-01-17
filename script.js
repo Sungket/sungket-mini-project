b6f9b9a2160cb6f1ccb8ab0ab483cb466d43aad1
Sungket <sungket_rhymes@hotmail.com>
2026-01-17 16:36:54 +0000

    added empty game loop function

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-// let gameboard = [[A00, A01, A02],
-//                  [A10, A11, A12], 
-//                  [A20, A21, A22]];
+//build the gameboard
+let gameboard = [];
 
-// let A00 = document.getElementById("A1");
-// let A01 = document.getElementById("A2");
-// let A02 = document.getElementById("A3");
+function buildGameboard() {
+    let rows = 3;
+    let cols = 3;
+    for (let i = 0; i < rows; i++) {
+        gameboard[i] = [];
+        for (let j = 0; j < cols; j++) {
+            gameboard[i][j] = ["."];
+        }
+    }
+}
 
-// let A10 = document.getElementById("B1");
-// let A11 = document.getElementById("B2");
-// let A12 = document.getElementById("B3");
+buildGameboard();
 
-// let A20 = document.getElementById("C1");
-// let A21 = document.getElementById("C2");
-// let A22 = document.getElementById("C3");
-
-// let box = document.getElementById("1");
-// let box = document.querySelector("div"); //this selects the first parent div
+//game loop
+function run() {
+    while (!winningCondition) {
+        
+    }
+}
 
 const divs = document.querySelectorAll(".box");
 
-divs.forEach((div) => {
-    div.addEventListener("click", () => {
+let currentPlayer = "O";
+
+if (currentPlayer === "O") {
+    divs.forEach((div) => {
+        div.addEventListener("click", () => {
+        console.log(div.id);
+        div.innerHTML = "O";
+        console.log(gameboard);
+        currentPlayer = "X";
+        })
+    })
+} else {
+    divs.forEach((div) => {
+        div.addEventListener("click", () => {
         console.log(div.id);
         div.innerHTML = "X";
+        console.log(gameboard);
+        currentPlayer = "O";
+        })
     })
-});
+}
+
+function winningCondition() {
+    //8 distinct winning conditions, which can be hardcoded in
+    
+}
 
 box.addEventListener("mouseover", () => {
     box.style.backgroundColor = "red";
