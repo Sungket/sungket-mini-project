e30aaeb03e61c9871479cd527c136c5446419190
Sungket <sungket_rhymes@hotmail.com>
2026-01-17 13:52:51 +0000

    got the clicks to register the ids of the cells

--- script.js
+++ script.js
@@ -14,9 +14,22 @@
 // let A21 = document.getElementById("C2");
 // let A22 = document.getElementById("C3");
 
-const box = document.getElementById("box");
+// let box = document.getElementById("1");
+// let box = document.querySelector("div"); //this selects the first parent div
 
-function addImage() {
-    
-}
+const divs = document.querySelectorAll(".box");
+
+divs.forEach((div) => {
+    div.addEventListener("click", () => {
+        console.log(div.id);
+    })
+})
+
+box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = "red";
+});
+
+box.addEventListener("mouseout", () => {
+    box.style.backgroundColor = "white";
+});
 
